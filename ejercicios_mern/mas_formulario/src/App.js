4872005cdd32e82817f8c9753a44a3d02a4689ae
Danilorca <daniela.lorca.r@gmail.com>
2022-10-09 19:10:34 -0300

    ejercicio de formulario resuelto con formik

---
+++ ejercicios_mern/mas_formulario/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Register from "./view/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form-container">
+        <Register></Register>
+      </div>
+    </div>
+  );
+}
+
+export default App;
